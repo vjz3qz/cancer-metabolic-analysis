285355238858c772c171d45b2b1df36094c35c07
vjz3qz <vjz3qz@virginia.edu>
2023-05-08 01:10:53 -0400

    cleaned up file tree

--- PythonScripts/.ipynb_checkpoints/test-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import sys
-filepath=sys.argv[1] # "/gpfs/gpfs0/scratch/vjz3qz/sp23-project/NormalizedCancerData/"+site+"/TSVFiles/"+site+str(i)+".tsv"
-file=filepath.split("/")[-1] # site+str(i)+".tsv"
-filename=file.split(".")[0] # site+str(i)
-outpath=filepath.replace(file,filename) # "/gpfs/gpfs0/scratch/vjz3qz/sp23-project/NormalizedCancerData/"+site+"/TSVFiles/"+site+str(i)
-outpath=outpath.replace("TSVFiles","RiptideOutputs") #"/gpfs/gpfs0/scratch/vjz3qz/sp23-project/NormalizedCancerData/"+site+"/RiptideOutputs/"+site+str(i)
-print(filepath)
-print(outpath)
